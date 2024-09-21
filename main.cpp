dd0672057d8dfdfb2af71cb8363cb8bc95a65686
sclin <read0651@hotmail.com>
2024-09-21 15:06:46 +0800

    1. Created function to draw all labels. 2. Displayed labels from cv::distanceTransform.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 
 #define DISPLAY_IMAGE 1
 // switch of different distance transform
-// #define USE_L1_DISTANCE 
 
 void DisplayImage(const cv::Mat& image, const std::string& title)
 {
@@ -25,55 +24,43 @@ void StoreMatToFile(const T& mat, const std::string& matName, const std::string&
     fs.release();
 }
 
-#if 0
-// This is the sample code from: https://answers.opencv.org/question/129819/finding-distance-between-two-curves/
-int main(int argc, const char** argv)
+bool IsMaskEmpty(const cv::Mat& mask)
 {
-	cv::Mat image = cv::imread("14878460214049233.jpg", cv::IMREAD_GRAYSCALE);
-	DisplayImage(image, "Source Image");
+    bool isMaskEmpty = false;
 
-	cv::Mat binaryImage;
-	cv::threshold(image, binaryImage, 200, 255, cv::THRESH_BINARY);
-	std::cout << "Binary image type = " << cv::typeToString(binaryImage.type()) << std::endl;
-	DisplayImage(binaryImage, "Binary Image");
+	if (cv::countNonZero(mask) == 0)
+        isMaskEmpty = true;
 
-	cv::Mat labels;
-	cv::connectedComponents(binaryImage, labels);
-	std::cout << "labels image type = " << cv::typeToString(labels.type()) << std::endl;
-	cv::Mat result(image.size(), CV_32FC1, cv::Scalar::all(0));
-    for (int i = 0; i <= 1; i++) {
-        cv::Mat mask1 = labels == 1+i;
-        cv::Mat mask2 = labels == 1+(1-i);
-        cv::Mat masknot;
-        bitwise_not(mask1,masknot);
-		DisplayImage(mask1, "Mask1");
-        cv::Mat dist;
-        cv::distanceTransform(masknot, dist, cv::DIST_L2, 5, CV_8U);
-		DisplayImage(dist / 255, "Distance Transform(distance float)");
-        dist.copyTo(result, mask2);
-    }
-	DisplayImage(result, "Distance");
-
-	StoreMatToFile(result, "Image", "distCtr.yml");
-
-    cv::SparseMat ms(result);
-	StoreMatToFile(ms, "Mat", "sparseMat.yml");
-    cv::SparseMatConstIterator_<float> it = ms.begin<float>(),it_end = ms.end<float>();
-    cv::Mat lig(result.rows, 1, CV_8U, cv::Scalar::all(0));
-    for (; it != it_end; it ++) {
-        // print element indices and the element value
-        const cv::SparseMat::Node* n = it.node();
-        if (lig.at<uchar>(n->idx[0])==0) {
-            std::cout << "(" << n->idx[0] << "," << n->idx[1] << ") = " << it.value<float>() << "\t";
-            lig.at<uchar>(n->idx[0])=1;
-        }
-    }
-
-	// std::cout << "\n\nlig: \n" << lig << std::endl;
-
-	return EXIT_SUCCESS;
+    return isMaskEmpty;
 }
-#else
+
+void DrawAllLabels(cv::Mat labels, cv::Mat& labelsResultImage)
+{
+    if (labels.type() != CV_32S) {
+        std::cerr << "The input image type is not CV_32S" << std::endl;
+        return;
+    }
+
+    cv::RNG rng(12345);
+    // cv::Mat labelResultImage = cv::Mat::zeros(labels.size(), CV_8UC3);
+    labelsResultImage = cv::Mat::zeros(labels.size(), CV_8UC3);
+    cv::Mat colorBackground;
+    cv::Mat oneLabel;
+    int label = 1; // label start from 1. zero is background
+    do {
+        oneLabel = labels == label;
+        if (IsMaskEmpty(oneLabel)) 
+            break;
+        
+        cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        colorBackground = cv::Mat(labels.size(), CV_8UC3, color);
+        colorBackground.copyTo(labelsResultImage, oneLabel);
+        label = label + 1;
+    } while (!IsMaskEmpty(oneLabel));
+
+    // DisplayImage(labelResultImage, "Label Result Image");
+}
+
 int main(int argc, const char** argv)
 {
 	cv::Mat image = cv::imread("NoDefect.bmp", cv::IMREAD_GRAYSCALE);
@@ -84,10 +71,14 @@ int main(int argc, const char** argv)
 	DisplayImage(edgeImg, "Edge Image");
 	cv::Mat edgeBinaryImage;
 	cv::threshold(edgeImg, edgeBinaryImage, 100, 255, cv::THRESH_BINARY);
-	DisplayImage(edgeBinaryImage, "Edge Binary Image");
+	// DisplayImage(edgeBinaryImage, "Edge Binary Image");
 
 	cv::Mat labels;
-	cv::connectedComponents(edgeBinaryImage, labels);
+	int num = cv::connectedComponents(edgeBinaryImage, labels);
+    std::cout << "labels image type = " << cv::typeToString(labels.type()) << ", label number = " << num << std::endl;
+    cv::Mat labelsResultImage;
+    DrawAllLabels(labels, labelsResultImage);
+    DisplayImage(labelsResultImage, "edges");
 	std::cout << "labels image type = " << cv::typeToString(labels.type()) << std::endl;
 	cv::Mat result(image.size(), CV_32FC1, cv::Scalar::all(0));
     for (int i = 0; i <= 1; i++) {
@@ -97,14 +88,14 @@ int main(int argc, const char** argv)
         bitwise_not(mask1,masknot);
 		DisplayImage(mask1, "Mask1");
 		DisplayImage(masknot, "masknot");
-        cv::Mat dist;
-#ifdef USE_L1_DISTANCE
-        cv::distanceTransform(masknot, dist, cv::DIST_L1, 5, CV_8U);
-		DisplayImage(dist, "Distance Transform(distance float)");
-#else
-        cv::distanceTransform(masknot, dist, cv::DIST_L2, 5, CV_8U);
+
+        cv::Mat dist, distLabels;
+        cv::distanceTransform(masknot, dist, distLabels, cv::DIST_L2, 5);
 		DisplayImage(dist / 255, "Distance Transform(distance float)");
-#endif
+        cv::Mat distLabelsResultImage;
+        DrawAllLabels(distLabels, distLabelsResultImage);
+        DisplayImage(distLabelsResultImage, "Distance Labels");
+
 		DisplayImage(mask2, "Mask2");
         dist.copyTo(result, mask2);
     }
@@ -114,21 +105,13 @@ int main(int argc, const char** argv)
 
     cv::SparseMat ms(result);
 	StoreMatToFile(ms, "Mat", "sparseMat.yml");
-#ifdef USE_L1_DISTANCE
-    cv::SparseMatConstIterator_<uchar> it = ms.begin<uchar>(),it_end = ms.end<uchar>();
-#else
     cv::SparseMatConstIterator_<float> it = ms.begin<float>(),it_end = ms.end<float>();
-#endif
     cv::Mat lig(result.rows, 1, CV_8U, cv::Scalar::all(0));
     for (; it != it_end; it ++) {
         // print element indices and the element value
         const cv::SparseMat::Node* n = it.node();
         if (lig.at<uchar>(n->idx[0])==0) {
-#ifdef USE_L1_DISTANCE
-            std::cout << "(" << n->idx[0] << "," << n->idx[1] << ") = " << (int)it.value<uchar>() << "\t";
-#else
             std::cout << "(" << n->idx[0] << "," << n->idx[1] << ") = " << it.value<float>() << "\t";
-#endif
             lig.at<uchar>(n->idx[0])=1;
         }
     }
@@ -137,4 +120,3 @@ int main(int argc, const char** argv)
 
 	return EXIT_SUCCESS;
 }
-#endif
